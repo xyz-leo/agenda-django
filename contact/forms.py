a063886f7e3f893eeb69bfc99312633cb2b80a9f
xyz-leo <leo.me9908@gmail.com>
2025-06-23 23:04:38 -0300

    minor changes in style.css and forms.py

--- contact/forms.py
+++ contact/forms.py
@@ -3,9 +3,17 @@ from django.core.exceptions import ValidationError
 from . import models
 
 class ContactForm(forms.ModelForm):
+    #def __init__(self, *args, **kwargs):
+    #    super().__init__(*args, **kwargs)
+    #    self.fields['email'].widget.attrs.update({'placeholder':'Enter your e-mail'})
+
     class Meta:
         model = models.Contact
         fields = ('first_name', 'last_name', 'phone', 'email',)
+
+        #widgets = {
+        #        'email': forms.EmailInput(attrs={'placeholder': 'Enter your e-mail address'}),
+        #        }
     #def clean(self):
     #    cleaned_data = self.cleaned_data
     #    self.add_error('first_name', ValidationError('Error message', code='Invalid'))
