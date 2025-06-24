80f7af6de60a668671d4745cc9aa3884743e74b6
xyz-leo <leo.me9908@gmail.com>
2025-06-24 18:19:11 +0000

    Added some basic validations to the new contact form and changed the logic behind the for loop in create html

--- contact/forms.py
+++ contact/forms.py
@@ -3,20 +3,24 @@ from django.core.exceptions import ValidationError
 from . import models
 
 class ContactForm(forms.ModelForm):
-    #def __init__(self, *args, **kwargs):
-    #    super().__init__(*args, **kwargs)
-    #    self.fields['email'].widget.attrs.update({'placeholder':'Enter your e-mail'})
-
     class Meta:
         model = models.Contact
-        fields = ('first_name', 'last_name', 'phone', 'email',)
+        fields = ('first_name', 'last_name', 'phone', 'email', 'description', 'category',)
 
-        #widgets = {
-        #        'email': forms.EmailInput(attrs={'placeholder': 'Enter your e-mail address'}),
-        #        }
-    #def clean(self):
-    #    cleaned_data = self.cleaned_data
-    #    self.add_error('first_name', ValidationError('Error message', code='Invalid'))
-    #    return super().clean()
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        if any(char.isdigit() for char in first_name):
+            raise forms.ValidationError('First name cannot contain numbers.')
+        return first_name
 
+    def clean_last_name(self):
+        last_name = self.cleaned_data.get('last_name')
+        if any(char.isdigit() for char in last_name):
+            raise forms.ValidationError('Last name cannot contain numbers.')
+        return last_name
 
+    def clean_phone(self):
+        phone = self.cleaned_data.get('phone')
+        if not phone.isdigit():
+            raise forms.ValidationError('Phone must contain only numbers.')
+        return phone
